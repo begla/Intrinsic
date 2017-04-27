c83f3430b39c65991926203f21756c3b31db9f57
Benjamin Glatzel <begla@intrinsic-engine.com>
2017-04-27 12:14:39 +0200

    Also "style" the splash screen of IntrinsicEd

--- IntrinsicEd/src/main.cpp
+++ IntrinsicEd/src/main.cpp
@@ -34,6 +34,36 @@ int _main(int argc, char* argv[])
   QCoreApplication::setApplicationName("Intrinsic");
   QCoreApplication::setApplicationVersion("1.0.0");
 
+  // Style
+  {
+    qApp->setStyle(QStyleFactory::create("Fusion"));
+
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(37, 41, 45));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(32, 36, 40));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(35, 35, 35));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(236, 112, 0));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    a.setPalette(darkPalette);
+    a.setStyleSheet("QToolTip { color: #ffffff; background-color: #202428; "
+                    "border: 1px solid #EC7000; }");
+
+    int id = QFontDatabase::addApplicationFont(":/Fonts/fira");
+    QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+    QFont monospace(family);
+    monospace.setPointSize(10);
+    a.setFont(monospace);
+  }
+
   QPixmap splashscreen(":/Media/splashscreen");
   _INTR_ASSERT(!splashscreen.isNull());
 
