d98e116f70af3a7eaed81527bc35585a74991181
Benjamin Glatzel <begla@intrinsic-engine.com>
2017-04-29 17:35:05 +0200

    Fix: Typo in the distro create script

--- scripts_win32/DistroCreate.bat
+++ scripts_win32/DistroCreate.bat
@@ -1,17 +1,14 @@
 cd ..
 
-mkdir distro
-mkdir distro/config
-
 robocopy app/ distro/ *.dll
 robocopy app/ distro/ Intrinsic.exe
 robocopy app/ distro/ Intrinsic.pdb
 robocopy app/ distro/ IntrinsicEd.exe
 robocopy app/ distro/ IntrinsicEd.pdb
+robocopy app/ distro/ settings.json
 robocopy app/config/ distro/config/ material_pass_config.json
 robocopy app/config/ distro/config/ renderer_config.json
 robocopy app/config/ distro/config/ renderer_config_simple.json
-robocopy app/ distro/ settings.json
 robocopy app/platforms/ distro/platforms /E
 robocopy app/managers/ distro/managers /E
 robocopy app/media/ distro/media /E
