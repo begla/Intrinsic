47de6483b29d98628eea076445638de02febb46d
Benjamin Glatzel <begla@intrinsic-engine.com>
2017-05-11 06:12:14 +0200

    Updated plot

--- docs/pyplots/volumetrics.py
+++ docs/pyplots/volumetrics.py
@@ -6,10 +6,10 @@ near = 1.0
 far = 10000.0
 volumeDepth = 128.0
 
-def volumeZToDepth(depth):
-    return np.power(depth, exp) * far + near
+def volumeZToDepth(z):
+    return np.power(z / volumeDepth, exp) * far + near
 
-t1 = np.arange(0.0, 1.0, 0.1)
+t1 = np.arange(0.0, volumeDepth, 1.0)
 
 plt.plot(t1, volumeZToDepth(t1), 'bo', t1, volumeZToDepth(t1), 'k')
 plt.ylabel('Depth')
