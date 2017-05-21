8647d22aed86d7866ed20053a950d764d03a9283
Benjamin Glatzel <begla@intrinsic-engine.com>
2017-05-21 22:04:52 +0200

    Some style tweaks and show the version number in the splash screen

--- IntrinsicEd/src/main.cpp
+++ IntrinsicEd/src/main.cpp
@@ -28,7 +28,8 @@ QSplashScreen* splash = nullptr;
 
 void onLoggedSplashscreen(const char* p_Message, Log::LogLevel::Enum p_LogLevel)
 {
-  splash->showMessage(p_Message, Qt::AlignBottom | Qt::AlignLeft, Qt::white);
+  splash->showMessage(QString(_INTR_VERSION_STRING) + " | " + p_Message,
+                      Qt::AlignBottom | Qt::AlignLeft, Qt::white);
 }
 
 int _main(int argc, char* argv[])
@@ -46,21 +47,22 @@ int _main(int argc, char* argv[])
     QPalette darkPalette;
     darkPalette.setColor(QPalette::Window, QColor(37, 41, 45));
     darkPalette.setColor(QPalette::WindowText, Qt::white);
-    darkPalette.setColor(QPalette::Base, QColor(32, 36, 40));
-    darkPalette.setColor(QPalette::AlternateBase, QColor(35, 35, 35));
+    darkPalette.setColor(QPalette::Base, QColor(42, 46, 50));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(42, 46, 50));
+
     darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
     darkPalette.setColor(QPalette::ToolTipText, Qt::white);
     darkPalette.setColor(QPalette::Text, Qt::white);
     darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
     darkPalette.setColor(QPalette::ButtonText, Qt::white);
     darkPalette.setColor(QPalette::BrightText, Qt::red);
-    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Link, QColor(236, 112, 0));
     darkPalette.setColor(QPalette::Highlight, QColor(236, 112, 0));
     darkPalette.setColor(QPalette::HighlightedText, Qt::black);
 
-    a.setPalette(darkPalette);
     a.setStyleSheet("QToolTip { color: #ffffff; background-color: #202428; "
-                    "border: 1px solid #EC7000; }");
+                    "border: 1px solid #EC7000; } QToolBar { border: 0px }");
+    a.setPalette(darkPalette);
 
     int id = QFontDatabase::addApplicationFont(":/Fonts/fira");
     QString family = QFontDatabase::applicationFontFamilies(id).at(0);
