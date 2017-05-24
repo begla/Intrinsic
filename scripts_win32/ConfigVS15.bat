e8628c3e6acfa0e113629a37c4acd44e2ea9b739
Benjamin Glatzel <begla@intrinsic-engine.com>
2017-05-24 23:48:22 +0200

    Added scripts for VS15 (Visual Studio 2017)

---
+++ scripts_win32/ConfigVS15.bat
@@ -0,0 +1,9 @@
+cd ..
+mkdir build
+cd build
+
+cmake -G"Visual Studio 15 2017 Win64" ..
+
+cd ..\scripts_win32
+
+timeout 2
---
+++ scripts_win32/ConfigVS15_FinalBuild.bat
@@ -0,0 +1,9 @@
+cd ..
+mkdir build
+cd build
+
+cmake -DINTR_FINAL_BUILD:BOOL=ON -G"Visual Studio 15 2017 Win64" ..
+
+cd ..\scripts_win32
+
+timeout 2
