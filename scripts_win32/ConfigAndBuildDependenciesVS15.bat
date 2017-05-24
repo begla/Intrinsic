fd6e40fde519077ebe977df48424f9e1ac49bf8d
Benjamin Glatzel <begla@intrinsic-engine.com>
2017-05-24 23:48:22 +0200

    Added scripts for VS15 (Visual Studio 2017)

---
+++ scripts_win32/ConfigAndBuildDependenciesVS15.bat
@@ -0,0 +1,11 @@
+cd ../dependencies/glslang
+mkdir build
+cd build
+
+cmake .. -G"Visual Studio 15 2017 Win64"
+call cmake --build . --config Release
+call cmake --build . --config Debug
+
+cd ..\..\..\scripts_win32
+
+timeout 2
