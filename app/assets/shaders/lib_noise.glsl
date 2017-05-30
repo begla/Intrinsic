bea75ad093e81fe5a2c4229dd786f431b7328fe1
Benjamin Glatzel <benjamin.glatzel@me.com>
2017-05-30 11:19:09 +0200

    Further improved veg. animation

--- app/assets/shaders/lib_noise.glsl
+++ app/assets/shaders/lib_noise.glsl
@@ -14,12 +14,11 @@
 
 float hash(float n) { return fract(sin(n) * 753.5453123); }
 
-const float windStrengthFreq = 0.25;
-const float grassFreq = 5.0;
-const float foliageFreq = 2.0;
-const float grassIntensFact = 0.1;
-const float foliageIntensFact = 0.5;
-const float minWindStrength = 0.2;
+const float windStrengthFreq = 0.1;
+const float grassFreq = 3.0;
+const float foliageFreq = 0.25;
+const float grassIntensFact = 0.5;
+const float foliageIntensFact = 0.75;
 
 float noise(in vec3 x)
 {
@@ -41,7 +40,17 @@ vec4 smoothCurve(vec4 x)
   return x * x * (3.0 - 2.0 * x);  
 }
 
+float smoothCurve(float x)
+{  
+  return x * x * (3.0 - 2.0 * x);  
+}
+
 vec4 triangleWave(vec4 x) 
+{  
+  return abs(fract(x + 0.5) * 2.0 - 1.0);  
+}
+
+float triangleWave(float x) 
 {  
   return abs(fract(x + 0.5) * 2.0 - 1.0);  
 }
@@ -49,7 +58,12 @@ vec4 triangleWave(vec4 x)
 vec4 smoothTriangleWave(vec4 x) 
 {  
   return smoothCurve(triangleWave(x));  
-}  
+}
+
+float smoothTriangleWave(float x) 
+{  
+  return smoothCurve(triangleWave(x));  
+} 
 
 #define SIDE_TO_SIDE_FREQ1 1.975
 #define SIDE_TO_SIDE_FREQ2 0.793
@@ -60,35 +74,35 @@ void applyDetailBending(
   inout vec3 vPos,
   vec3 vNormal,
   vec3 objectPosition,
-  float fDetailPhase,
-  float fBranchPhase,
-  float fTime,
-  float fEdgeAtten,
-  float fBranchAtten,
-  float fBranchAmp,
-  float fSpeed,
-  float fDetailFreq,
-  float fDetailAmp)
+  float detailPhase,
+  float branchPhase,
+  float time,
+  float edgeAtten,
+  float branchAtten,
+  float branchAmp,
+  float speed,
+  float detailFreq,
+  float detailAmp)
 {
-  float fObjPhase = dot(objectPosition.xyz, vec3(1.0));  
-  fBranchPhase += fObjPhase;
-  float fVtxPhase = dot(vPos.xyz, vec3(fDetailPhase + fBranchPhase));  
+  const float objPhase = dot(objectPosition.xyz, vec3(1.0));  
+  branchPhase += objPhase;
+  float vtxPhase = dot(vPos.xyz, vec3(detailPhase + branchPhase));  
 
-  vec2 vWavesIn = fTime + vec2(fVtxPhase, fBranchPhase);
-  vec4 vWaves = (fract(vWavesIn.xxyy *
+  const vec2 wavesIn = time + vec2(vtxPhase, branchPhase);
+  vec4 waves = (fract(wavesIn.xxyy *
              vec4(SIDE_TO_SIDE_FREQ1, SIDE_TO_SIDE_FREQ2, 
                   UP_AND_DOWN_FREQ1, UP_AND_DOWN_FREQ2)) *
-             2.0 - 1.0) * fSpeed * fDetailFreq;
-  vWaves = smoothTriangleWave(vWaves);
-  vec2 vWavesSum = vWaves.xz + vWaves.yw;  
+             2.0 - 1.0) * speed * detailFreq;
+  waves = smoothTriangleWave(waves);
+  const vec2 wavesSum = waves.xz + waves.yw;  
 
-  vPos.xyz += vWavesSum.x * vec3(fEdgeAtten * fDetailAmp * vNormal.xyz);
-  vPos.y += vWavesSum.y * fBranchAtten * fBranchAmp;
+  vPos.xyz += wavesSum.x * vec3(edgeAtten * detailAmp * vNormal.xyz);
+  vPos.y += wavesSum.y * branchAtten * branchAmp;
 }
 
 void applyMainBending(inout vec3 pos, vec2 wind, float bendScale)
 {
-  float length = length(pos);
+  const float length = length(pos);
   float bf = pos.y * bendScale;
   bf += 1.0;
   bf *= bf;
@@ -99,13 +113,21 @@ void applyMainBending(inout vec3 pos, vec2 wind, float bendScale)
 }
 // <-
 
+void applyGrassBending(inout vec3 pos, vec2 wind)
+{
+  const float falloff = clamp(pos.y * pos.y, 0.0, 1.0);
+  const float windLength = length(wind);
+  const vec3 windDir = vec3(wind.x, -windLength * windLength, wind.y);
+  pos.xyz += falloff * windDir;
+}
+
 vec2 calcWindStrength(float time)
 {
-  return max(
-    vec2(noise(vec3(time * windStrengthFreq, 0.0, 0.0)),
-      noise(vec3(time * windStrengthFreq + 0.5891, 0.0, 0.0)))
-      * 2.0 - 1.0,
-    vec2(minWindStrength));
+  return clamp(
+    vec2(
+      noise(vec3(time * windStrengthFreq, 0.0, 0.0)), 
+      noise(vec3(0.0, time * windStrengthFreq, 0.0))
+    ), 0.0, 1.0) * 2.0 - 1.0;
 }
 
 void applyTreeWind(
@@ -118,8 +140,8 @@ void applyTreeWind(
 {
   const vec2 wind = foliageIntensFact
     * windStrength
-    * vec2(sin(worldPos.x * 0.05 + time * foliageFreq));
-  const float windS = length(windStrength);
+    * clamp(vec2(smoothTriangleWave(time * foliageFreq)) * 0.4 + 0.6, 0.0, 1.0);
+  const float windS = clamp(length(windStrength), 0.0, 1.0);
 
   applyDetailBending(localPos, worldNormal, worldPos,
     0.0, 0.0, time, intensity, intensity, windS * 5.0, 1.0, 0.1, 
@@ -133,9 +155,22 @@ void applyGrassWind(
   float time,
   vec2 windStrength)
 {
+  const vec2 detailStrength = clamp(
+      vec2(
+        noise(vec3(worldPos.x * 2.0 + time * grassFreq, 0.0, 0.0)) * 0.5 + 0.5,
+        noise(vec3(worldPos.z * 2.0 + time * grassFreq, 0.0, 0.0)) * 0.5 + 0.5
+      )
+      , vec2(0.0), vec2(1.0));
+  const vec2 mainStrength = clamp(
+    vec2(
+        (smoothTriangleWave(worldPos.x * 0.05 + time * grassFreq * 0.25) * 0.5 + 0.5) * 0.8 + 0.2,
+        (smoothTriangleWave(worldPos.z * 0.05 + time * grassFreq * 0.25) * 0.5 + 0.5) * 0.8 + 0.2
+      )
+      , vec2(0.0), vec2(1.0));
+
   const vec2 wind = grassIntensFact
     * windStrength
-    * clamp(sin(worldPos.x * 0.1 + time * grassFreq) * 0.5 + 0.5, 0.0, 1.0);
+    * ((mainStrength + detailStrength) * 0.5);
 
-  applyMainBending(localPos, wind, 0.8);
+  applyGrassBending(localPos, wind);
 } 
