9b19c003059092e629fe7f308f24474d53fbbe59
Benjamin Glatzel <begla@intrinsic-engine.com>
2017-06-11 17:23:13 +0200

    Updated version string

--- IntrinsicCore/src/IntrinsicCoreVersion.h
+++ IntrinsicCore/src/IntrinsicCoreVersion.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define _INTR_VERSION_STRING "v0.0.25"
+#define _INTR_VERSION_STRING "v0.0.26"
 #if defined(_WIN32)
-#define _INTR_VERSION_STRING_L L"v0.0.25"
+#define _INTR_VERSION_STRING_L L"v0.0.26"
 #endif // _WIN32
