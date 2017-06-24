0d3188f910e2c51e8d36e7bf2199c6c210620ba2
Benjamin Glatzel <begla@intrinsic-engine.com>
2017-06-24 20:00:56 +0200

    Updated version string

--- IntrinsicCore/src/IntrinsicCoreVersion.h
+++ IntrinsicCore/src/IntrinsicCoreVersion.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define _INTR_VERSION_STRING "v0.0.27"
+#define _INTR_VERSION_STRING "v0.0.28"
 #if defined(_WIN32)
-#define _INTR_VERSION_STRING_L L"v0.0.27"
+#define _INTR_VERSION_STRING_L L"v0.0.28"
 #endif // _WIN32
--- scripts_win32/RunCloc.bat
+++ scripts_win32/RunCloc.bat
@@ -1,3 +1,5 @@
 cd ..
 
 app\\tools\\cloc\\cloc-1.70.exe IntrinsicCore/src IntrinsicEd/src IntrinsicAssetManagement/src IntrinsicRendererVulkan/src Intrinsic/src
+
+timeout 10
